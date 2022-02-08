84d93ce341813f47a9d4bd4d2ffff64dcfaf0a16
Jasem Duncan <jasemvalencia@gmail.com>
2022-02-08 17:28:44 -0500

    Add an Action and a Reducer

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,13 @@
 import React from "react"
-import PropTypes from "prop-types"
 
 import { BrowserRouter, Switch, Route } from  'react-router-dom'
 
+import { Provider } from 'react-redux';
+
 import HelloWorld from './HelloWorld'
+
+import configureStore from '../configureStore'
+const store=configureStore();
 
 class App extends React.Component {
   render () {
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get '*page', to: 'static#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
   end 
+  
   root 'static#index'
 end
